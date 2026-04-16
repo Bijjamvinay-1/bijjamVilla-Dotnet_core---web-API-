4ca95352636a7d253951188df060c19a645346ab
bijjam.vinay <bijjam.vinay@accenture.com>
2026-04-16 18:07:16 +0530

    Route parameter Binding

---
+++ bijjamVilla (Dotnet_core - web API)/Controllers/VillaController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace bijjamVilla__Dotnet_core___web_API_.Controllers
+{
+    [Route("api/villas")]
+    [ApiController]
+    public class VillaController : ControllerBase      //if it is a MVC then we can use only Controller instead   of ControllerBase.
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "Get Villa";
+        }
+
+        [HttpGet("{id:Int}")]    // Route parameter Binding
+        public string GetVillas(int id)
+        {
+            return "Get Villa with id: " + id;
+        }
+    }
+}
