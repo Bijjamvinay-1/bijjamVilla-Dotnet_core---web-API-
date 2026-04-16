47194d507af73be105432a18887c1d7e782a21b6
bijjam.vinay <bijjam.vinay@accenture.com>
2026-04-16 11:32:49 +0530

    Add project files.

---
+++ bijjamVilla (Dotnet_core - web API)/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace bijjamVilla__Dotnet_core___web_API_
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
