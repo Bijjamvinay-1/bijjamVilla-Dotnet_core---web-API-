32370552eaf90bcd6827e23330326adfaa181d9b
bijjam.vinay <bijjam.vinay@accenture.com>
2026-04-21 19:10:12 +0530

    Static Factory Method - for stucture Api response

--- bijjamVilla (Dotnet_core - web API)/Controllers/VillaController.cs
+++ bijjamVilla (Dotnet_core - web API)/Controllers/VillaController.cs
@@ -21,10 +21,12 @@ namespace bijjamVilla__Dotnet_core___web_API_.Controllers
             _mapper = mapper;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<villaDTO>>> GetVillas()
+        public async Task<ActionResult<ApiResponse<IEnumerable<villaDTO>>>> GetVillas()
         {
             var villas = await _db.villa.ToListAsync();
-            return Ok(_mapper.Map<List<villaDTO>>(villas));
+            var dtoResponseVilla = _mapper.Map<List<villaDTO>>(villas);
+            var response = ApiResponse<IEnumerable<villaDTO>>.OK(dtoResponseVilla, "Villas retrieved successfully");
+            return Ok(response);
         }
 
         [HttpGet("{id:int}")]
@@ -34,29 +36,16 @@ namespace bijjamVilla__Dotnet_core___web_API_.Controllers
             {
                 if (id <= 0)
                 {
-                    return new ApiResponse<villaDTO>()
-                    {
-                        Success = false,
-                        Errors = "Villa ID must be greater than 0" ,
-                        StatusCode = StatusCodes.Status400BadRequest,
-                        Message = "Bad Request"
-                        
-                    };
+                    return NotFound(ApiResponse<villaDTO>.NotFound("Villa Id must be a positive integer"));
                 }
 
                 var villa = await _db.villa.FirstOrDefaultAsync(v => v.Id == id);
                 if (villa == null)
                 {
-                    return NotFound($"Villa with ID {id} Was not Found");
+                    return NotFound(ApiResponse<villaDTO>.NotFound($"Villa with ID {id} was not found"));
                 }
-                return new ApiResponse<villaDTO>()
-                {
-                    Success = true,
-                    StatusCode = StatusCodes.Status200OK,
-                    Message = $"Villa with ID {id} retrieved successfully",
-                    Data = _mapper.Map<villaDTO>(villa),
-                    Timestamp = DateTime.UtcNow
-                };
+                var dtoResponseVilla = _mapper.Map<villaDTO>(villa);
+                return Ok(ApiResponse<villaDTO>.OK(dtoResponseVilla, $"Villa with ID {id} retrieved successfully"));
             }
             catch (Exception ex)
             {
@@ -65,32 +54,33 @@ namespace bijjamVilla__Dotnet_core___web_API_.Controllers
             }
         }
         [HttpPost]
-        public async Task<ActionResult<villaDTO>> CreateVillas(villaCreateDTO villaDTO)
+        public async Task<ActionResult<ApiResponse<villaDTO>>> CreateVillas(villaCreateDTO villaDTO)
         {
             try
             {
                 if (villaDTO == null)
                 {
-                    return BadRequest("Villa data is required");
+                    return BadRequest(ApiResponse<villaDTO>.Badrequest("Villa data is required"));
                 }
 
                 var DuplicateVilla = await _db.villa.FirstOrDefaultAsync(u => u.Name.ToLower() == villaDTO.Name.ToLower());
 
                 if (DuplicateVilla != null)
                 {
-                    return Conflict($"Villa with name {villaDTO.Name} already exists");
+                    return Conflict(ApiResponse<villaDTO>.Conflict($"Villa with name {villaDTO.Name} already exists"));
                 }
 
                 villa Villa = _mapper.Map<villa>(villaDTO);
                     
                  await _db.villa.AddAsync(Villa);
                 await _db.SaveChangesAsync();
-                return CreatedAtAction(nameof(CreateVillas), new {id =Villa.Id},_mapper.Map<villaDTO>(Villa));
+                var response = ApiResponse<villaDTO>.CreatedAt(_mapper.Map<villaDTO>(Villa), "Villa created successfully");
+                return CreatedAtAction(nameof(CreateVillas), new {id =Villa.Id}, response);
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    $"Error occured while creating the  villa : {ex.Message}");
+                var errorResponse = ApiResponse<object>.Error(500, $"Error occured while creating the villa: {ex.Message}");
+                return StatusCode(500, errorResponse);
             }
         }
 
@@ -101,41 +91,42 @@ namespace bijjamVilla__Dotnet_core___web_API_.Controllers
             {
                 if (villaDTO == null)
                 {
-                    return BadRequest("Villa data is required");
+                    return BadRequest(ApiResponse<villaDTO>.Badrequest("Villa  can't be null value"));
                 }
 
                 if (id != villaDTO.Id)
                 {
-                    return BadRequest("Villa ID in url dose not match Villa ID in request body");
+                    return BadRequest(ApiResponse<villaDTO>.Badrequest("Villa ID in url dose not match Villa ID in request body"));
                 }
 
                 var existingVilla = await _db.villa.FirstOrDefaultAsync(u => u.Id == id);
 
                 if (existingVilla == null)
                 {
-                    return NotFound($"Villa with ID {id} Was not Found");
+                    return NotFound(ApiResponse<villaDTO>.NotFound($"Villa with ID {id} Was not Found"));
                 }
 
                 var DuplicateVilla = await _db .villa.FirstOrDefaultAsync(u => u.Name.ToLower() == villaDTO.Name.ToLower() && u.Id != id);
 
                 if (DuplicateVilla != null)
                 {
-                    return Conflict($"Villa with name {villaDTO.Name} already exists");
+                    return Conflict(ApiResponse<villaDTO>.Conflict($"Villa with name {villaDTO.Name} already exists"));
                 }
 
                 _mapper.Map(villaDTO, existingVilla);
                 existingVilla.UpdatedDate = DateTime.Now;
                 await _db.SaveChangesAsync();
-                return Ok(_mapper.Map<villaDTO>(existingVilla));
+                var dtoResponseVilla = _mapper.Map<villaDTO>(existingVilla);
+                return Ok(ApiResponse<villaDTO>.OK(dtoResponseVilla, "Villa updated successfully"));
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    $"Error occured while Updating the  villa : {ex.Message}");
+                var errorResponse = ApiResponse<object>.Error(500, $"Error occured while Updating the villa: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
             }
         }
         [HttpDelete("{id:int}")]
-        public async Task<ActionResult<villaDTO>> DeleteVillas(int id)
+        public async Task<ActionResult<ApiResponse<villaDTO>>> DeleteVillas(int id)
         {
             try
             { 
@@ -143,20 +134,19 @@ namespace bijjamVilla__Dotnet_core___web_API_.Controllers
 
                 if (existingVilla == null)
                 {
-                    return NotFound($"Villa with ID {id} Was not Found");
+                    return NotFound(ApiResponse<villaDTO>.NotFound($"Villa with ID {id} Was not Found"));
                 }
                 
                 _db.villa.Remove(existingVilla);
                 await _db.SaveChangesAsync();
-                return Ok(_mapper.Map<villaDTO>(existingVilla));
+                var response = ApiResponse<villaDTO>.Nocontent($"Villa with ID {id} deleted successfully");
+                return Ok(response);
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    $"Error occured while Deleting the  villa : {ex.Message}");
+                var errorResponse = ApiResponse<object>.Error(500, $"Error occured while Deleting the  villa : {ex.Message}");
+                return StatusCode(500, errorResponse);
             }
         }
     }
-    
-
 }
--- bijjamVilla (Dotnet_core - web API)/DTO/ApiResponse.cs
+++ bijjamVilla (Dotnet_core - web API)/DTO/ApiResponse.cs
@@ -8,5 +8,38 @@
         public TData? Data { get; set; }
         public object? Errors { get; set; } 
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;    // Set default value to current UTC time when the response is created.
+
+        public static ApiResponse<TData> Create(bool success, int statusCode, string message, TData? data = default, object? errors = null)
+        {
+            return new ApiResponse<TData>
+            {
+                Success = success,
+                StatusCode = statusCode,
+                Message = message,
+                Data = data,
+                Errors = errors
+            };
+        }
+
+        public static ApiResponse<TData> OK(TData data, string message) =>
+           Create(true, 200, message, data);
+
+        public static ApiResponse<TData> CreatedAt(TData data, string message) =>
+           Create(true, 201, message, data);
+
+        public static ApiResponse<TData> Nocontent(string message = "Operation completed successfully") =>
+            Create(false, 404, message);
+        public static ApiResponse<TData> NotFound(string message ="Resource not found") =>
+            Create(false, 404, message);
+
+        public static ApiResponse<TData> Badrequest(string message, object? errors = null) =>
+            Create(false, 400, message, errors: errors);
+
+        public static ApiResponse<TData> Conflict(string message) =>
+            Create(false, 409, message);
+
+        public static ApiResponse<TData> Error(int StatusCode, string message, object? errors = null) =>
+            Create(false, StatusCode, message, errors: errors);
     }
+
 }
